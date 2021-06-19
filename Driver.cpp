014f34c81a24a732dc34bf53eeecf76de75ecc10
Ushna-Naeem <84606670+Ushna-Naeem@users.noreply.github.com>
2021-06-20 04:29:06 +0500

    Update Driver.cpp

--- Driver.cpp
+++ Driver.cpp
@@ -1,12 +1,21 @@
 #include "Library.h"
+ HANDLE colour=GetStdHandle(STD_OUTPUT_HANDLE); 
 
 void main(){
-
+	//This is the main function to run the whole library mangement system 
 	Library Lb;
 	Lb.LoadData();
+	//Below is the start menu that will be showed on the screen and present the viewer with the options
 	while(1){
-		cout<<"\t\tWelcome to Library"<<endl;
-		cout<<"1) Librarian\n2) Member\n3) Quit"<<endl;
+		SetConsoleTextAttribute(colour, 10);
+		cout<<"----------------------------------------------------------------------\n"<<endl;
+		SetConsoleTextAttribute(colour, 6);
+		cout<<"\t\tWelcome to Library Management System!"<<endl;
+		SetConsoleTextAttribute(colour, 11);
+		cout<<endl<<" ~~ Kindly choose one of the following options: "<<endl;
+		cout<<"1) Enter the librarian domain.\n2) Enter the member domain.\n3) Quit the program."<<endl;
+		SetConsoleTextAttribute(colour, 5);
+		cout<<endl<<"Enter your choice: ";
 		switch (getch()){
 			case '1':
 				system("CLS");
@@ -19,12 +28,19 @@ void main(){
 			case '3':
 				system("CLS");
 				Lb.SaveData();
-				cout<<"You are Exiting!! THANK YOU for your visit!!\n\tHave a Great Day! :)"<<endl;
+				SetConsoleTextAttribute(colour, 10);
+				cout<<"-----------------------------------------------------------"<<endl;
+				SetConsoleTextAttribute(colour, 5);
+				cout<<endl<<"You are exiting the Library Management System!\n      Thank you for your visit!\n\t";
+				SetConsoleTextAttribute(colour, 6);
+				cout<<"Have a blessed day! :)"<<endl<<endl;
+				SetConsoleTextAttribute(colour, 10);
+				cout<<"-----------------------------------------------------------"<<endl;
 				system("pause");
 				return;
 			default:
 				system("CLS");
-				cout<<"Invalid input. Enter again." <<endl<<endl;
+				cout<<"Invalid input :( Enter again!" <<endl<<endl;
 				break;
 		}
 	}
